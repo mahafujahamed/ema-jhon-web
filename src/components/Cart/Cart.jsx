49df86b45f44979fc72db856d071d7c64bdbd565
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-03-31 21:17:37 +0600

    solve problem on shop file

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import './Cart.css';
-const Cart = (props) => {
-    const {cart} = props; //distructuring
-    let total = 0;
+
+const Cart = ({ cart }) => {
+    // const cart = props.cart; // option 1
+    // const {cart} = props; // option 2
+
+    // console.log(cart);
+
+    let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        total = total + product.price;
+        // if(product.quantity === 0){
+        //     product.quantity = 1;
+        // }
+        // product.quantity = product.quantity || 1;
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
-    const tax = total*5/100;
-    const grandTotal = total + totalShipping + tax;
+    const tax = totalPrice * 7 / 100;
+
+    const grandTotal = totalPrice + totalShipping + tax;
+
     return (
         <div className='cart'>
-            <h4>Order Sammary</h4>
-            <p>Selected Items: {cart.length}</p>
-            <p>Total Price: ${total}</p>
-            <p>Total Shipping Charge: ${totalShipping}</p>
+            <h4>Order Summary</h4>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
-            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
+            <h6>Grand Total: ${grandTotal.toFixed(2)} </h6>
         </div>
     );
 };
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import './Product.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
+
 const Product = (props) => {
-    const {img, name, seller, ratings, price, quantity} = props.product;
-    const addToCart = props.addToCart;
+
+    const { img, name, seller, ratings, price } = props.product;
+    const handleAddToCart = props.handleAddToCart;
+
+
     return (
         <div className='product'>
             <img src={img} alt="" />
             <div className='product-info'>
-            <h5 className='product-name'>{name}</h5>
-            <p>Price: ${price}</p>
-            <p>Manufacturer: {seller}</p>
-            <p>Rating: {ratings} stars</p>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings} Stars</p>
             </div>
-            <button onClick={() => addToCart(props.product
-                )} className='btn-cart'>
-                    Add Cart
-                    <FontAwesomeIcon icon={faCartShopping} />
-                    </button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
+                Add to Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
         </div>
     );
 };
